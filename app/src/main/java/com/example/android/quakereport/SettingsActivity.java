8acaafb6a9b59447e139ff72ffea32758694b5f5
Bots <botsone@gmail.com>
2018-02-21 15:52:18 -0700

    Added preference screen with beginnings of minMagnitude preference

---
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.quakereport;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bots on 2/21/18.
+ */
+
+public class SettingsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
+    }
+}
